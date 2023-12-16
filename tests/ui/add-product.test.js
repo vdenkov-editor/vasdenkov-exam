bcfd4aca0bf150aa2d1a554a7bd88b55dae04636
vdenkov-editor <vdenkov1312@gmail.com>
2023-12-16 13:46:29 +0100

    Fix UI test endpoints

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-product');
+    await page.goto('http://vasdenkov-exam.onrender.com/add-product');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
